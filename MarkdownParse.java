a770917df0564ac4e459fe4f10047e1ecc7549de
Cynthia B <cindysbao@gmail.com>
2022-04-14 10:42:16 -0700

    Committing Parts 1 and 2

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,12 +12,14 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            //System.out.println(currentIndex + " start");
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            //System.out.println(currentIndex + " end");
         }
 
         return toReturn;
