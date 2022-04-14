65472a6d11a30dbd77393a0eceb82df884ecd232
Cynthia B <cindysbao@gmail.com>
2022-04-14 11:11:47 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,14 +12,22 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            //no more links
+            //no more links/brackets
             if(markdown.indexOf("[", currentIndex)==-1){
                 break;
             }
             //System.out.println(currentIndex + " start");
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
+            //no parenthesis
+            if(markdown.indexOf("(", closeBracket)==-1){
+                return null;
+            }
             int openParen = markdown.indexOf("(", closeBracket);
+            //no parenthesis
+            if(markdown.indexOf(")", openBracket)==-1){
+                return null;
+            }
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
@@ -34,6 +42,8 @@ public class MarkdownParse {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        if(links != null){
+            System.out.println(links);
+        }
     }
 }
