8c7b4f05027c463133c36c3e6c26e6c6ec4d2918
Cynthia B <cindysbao@gmail.com>
2022-04-21 11:06:16 -0700

    Create MarkdownParseTest.java

---
+++ MarkdownParseTest.java
@@ -0,0 +1,36 @@
+//importing junit library
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+public class MarkdownParseTest {
+
+    // @Before
+    // public void setup() {
+    //     mdp = new MarkdownParse();
+    // }
+
+    @Test //following method is a test
+    public void addition() { //testing addition
+        //checking to see if 1+1 = expected output
+        //of 2
+        assertEquals(2, 1+1);
+    }
+
+    @Test
+    public void testGetLinks() throws IOException {
+        ArrayList<String> testFileLink = new ArrayList<>();
+        testFileLink.addAll(Arrays.asList("https://something.com", "some-thing.html"));
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        assertEquals(testFileLink, MarkdownParse.getLinks(content)); 
+    }
+}
